39cec6b69d286436fc47bf955f15a026170464e9
anin <antony.inian@zuhlke.com>
2020-12-10 17:54:46 +0800

    Add please sign in test and nav.test

---
+++ sick-fits/frontend/__tests__/nav.test.js
@@ -0,0 +1,45 @@
+import { mount } from 'enzyme';
+import wait from 'waait';
+import Nav from '../components/Nav';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeUser } from '../lib/testUtils';
+import toJSON from 'enzyme-to-json';
+
+const notSignedInMocks = [{
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } }
+}];
+
+const signedInMocks = [{
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } }
+}];
+
+describe("<Nav/>", () => {
+    it("renders a minimal nav when signed out", async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={notSignedInMocks}>
+                <Nav />
+            </MockedProvider>
+        );
+        await wait();
+        wrapper.update();
+        wrapper.find('NavStyles')
+        expect(toJSON(wrapper.find('NavStyles'))).toMatchSnapshot();
+    });
+
+    it("renders full nav when signed in", async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={signedInMocks}>
+                <Nav />
+            </MockedProvider>
+        );
+        await wait();
+        wrapper.update();
+        console.log(wrapper.debug());
+        console.log(wrapper.find('NavStyles').debug());
+        wrapper.find('NavStyles')
+        expect(toJSON(wrapper.find('Link'))).toMatchSnapshot();
+    });
+});
